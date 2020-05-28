4db65de15c07af72d926a967cfbab1687606d251
Carl Padilla <carlpadilla@protonmail.com>
2020-05-27 22:22:34 -0400

    saving progress

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,4 +1,6 @@
 """
+
+O(log n)
 Binary search trees are a data structure that enforce an ordering over 
 the data they store. That ordering in turn makes it a lot more efficient 
 at searching for a particular piece of data in the tree. 
@@ -9,6 +11,8 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -17,10 +21,31 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+
+        if value >= self.value:
+            # check if. right exists
+            if self.right is not None:
+                # if so, make that node call insert with the same value
+                self.right.insert(value)
+
+            # if not, create a node with that value, set node as right child
+            else:
+                new_node = BSTNode(value)
+                self.right = new_node
+
+        # else, go left
+        else:
+            # check if left exits
+            if self.left:
+                # if so, make that node call insert with the same value
+
+                self.left.insert(value)
+
+            # if not, create a node right here
 
     # Return True if the tree contains the value
     # False if it does not
+
     def contains(self, target):
         pass
 
